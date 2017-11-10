29429eb8b413164b26b11b54c1aacd53749fc82c
CBanvard <33142678+CBanvard@users.noreply.github.com>
2017-11-10 09:52:47 -0500

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,22 @@
+class Deck
+	def __init__(self, cards)
+
+
+	def Shuffling(self, cards, amountOfCards)
+
+	
+	def Dealing(self, 
+
+
+
+class Card
+
+
+
+class Hands
+
+
+
+class player1
+
+class player2
