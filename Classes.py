d37668f0c61c16f06874a9aac8a1a70b7a6fc8d1
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-15 13:56:59 -0500

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,12 @@
+class Table #contains collection comparison logic
+
+class Deck #52 playing card deck, 2-A, no jokers
+
+class Hand #each player's hand, contains 5 cards
+
+class Player #each player, 2 in the game
+
+class Collection #each collection of cards, 5 per player
+
+class Card #each standard playing card
+
