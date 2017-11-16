c9e94d242d346e349b4a4c9101a99102f3461020
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-15 23:34:50 -0500

    Add files via upload

---
+++ collection.py
@@ -0,0 +1,4 @@
+class Collection
+
+    def __init__(self, current_card_num)
+        self.current_card_num = current_card_num
---
+++ table.py
@@ -0,0 +1,9 @@
+import evaluator
+
+class Table
+
+    def __init__(self):
+        self.num_of_players = 2
+        
+    def whoWins(self):
+        #evaluator logic
