36085c0e30b2c0d6e710f5dcd60a5e8f4498f3ab
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-25 13:08:09 -0500

    Update table.py

--- table.py
+++ table.py
@@ -5,6 +5,12 @@ class Table
     def __init__(self):
         self.P1 = Player()
         self.P2 = Player()
-        
+
+    def gameFlow(self):
+    #maybe this should be in the main function?
+        for i in range(5)
+            P1.selectcard()
+            P2.selectcard()
+    
     def whoWins(self):
-        #evaluator logic
+#evaluator logic
