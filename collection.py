71d4bc010aa777f54d77ba59a7d38be809363622
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-25 13:07:43 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -1,4 +1,4 @@
 class Collection
 
-    def __init__(self, current_card_num)
-        self.current_card_num = current_card_num
+    def __init__(self, current_card_num):
+        self.collection = []
