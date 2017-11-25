aa67fa55986b126724664858ddb0845cbc30a272
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-25 13:06:59 -0500

    Update hand.py

--- hand.py
+++ hand.py
@@ -1,4 +1,14 @@
+import card
+import deck
 class Hand
 
-    def __init__(self)
+    def __init__(self):
         self.numofcards = 5
+        self.hand = []
+
+    def deal(self):
+    #Have to figure out how to deal a random card, will examine deuces library more to figure it out
+        if len(hand) < numofcards:
+            hand = hand + card
+        else:
+            print("Hand is full.")
