2b5287b6ff5f71bc58d848585bf7643c8c9854b7
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-30 20:25:58 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -1,10 +1,15 @@
-import card
+from deuces import deck
 import player
 import hand
-class Collection
+class Collection:
 
     def __init__(self):
         self.collection = []
+        self.already_played = 0
 
     def addCard(self, card):
-        self.collection.append(card)
+        if table.round_num = already_played:
+            self.collection.append(card)
+            self.already_played += 1
+        else:
+            print("Cannot play in this collection.")
