e355479bf07472b43eb2687e8459d3c991bd1df4
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-30 20:27:47 -0500

    Update table.py

--- table.py
+++ table.py
@@ -1,21 +1,42 @@
-import evaluator
+from deuces import evaluator
+from deuces import deck
 import hand
-import card
-import deck
 import collection
+import player
 
-class Table
+class Table:
 
     def __init__(self):
-        self.P1 = Player()
-        self.P2 = Player()
+        self.P1 = player.Player()
+        self.P2 = player.Player()
+        self.round_num = 0
 
-    def gameFlow()
+    def gameFlow(self):
         for i in range(5):
-            P1.selectCollection()
-            P1.selectCard()
-            P2.selectCollection()
-            P2.selectCard()
+            self.round_num += 1
+            for i in range(5):
+                self.P1.selectCard()
+                self.P2.selectCard()
     
     def whoWins(self):
-#evaluator logic
+        evaluator = Evaluator()
+
+        P1_hands_won = 0
+        P2_hands_won = 0
+        
+        for i in range(5):
+            if evaluator.evaluate(P1.collection[i]) > P2.collection[i]):
+                P1_hands_won += 1
+            elif evaluator.evaluate(P1.collection[i]) < P2.collection[i]):
+                P2_hands_won += 1
+            else:
+                print("There was a tie.")
+
+        if P1_hands_won > P2_hands_won:
+            print("Player One Triumphs!")
+
+        elif P1_hands_won < P2_hands_won:
+            print("Player Two Prevails!")
+        
+        else:
+            print("There was a tie...")
