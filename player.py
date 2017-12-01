7a7a32da40110c89c1a3c2c8cdeff27097826bfe
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-30 20:27:30 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,23 +1,22 @@
 import hand
-import card
 import collection
-class Player
+class Player:
 
     def __init__(self):
         self.collection = []
         for i in range(5):
-            self.collection.append(Collection())
-        self.hand = Hand()
+            self.collection.append(collection.Collection())
+        self.hand = hand.Hand()
 
     def playCard(self, card, collectionchoice):
         self.collection[collectionchoice-1].addCard(card)
+        del hand[cardchoice-1]
 
     def selectCollection(self, collectionchoice):
         return self.collection[collectionchoice - 1]
 
-    def selectCard(self, hand, cardchoice)
-                  
-            if 1 <= charchoice <= 5:
-                  print("Please input a valid choice.")
-            else:
-                  playCard(hand[cardchoice-1], selectCollection())
+    def selectCard(self, hand, cardchoice):
+        if 1<=cardchoice<=5:
+            playCard(hand[cardchoice-1], selectCollection())
+        else:
+            print("Please input valid card choice.")
