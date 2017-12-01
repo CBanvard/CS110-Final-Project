3ef6b4aaea0f7e481f034d7237bca69540c7b54a
maevefarrell <33137646+maevefarrell@users.noreply.github.com>
2017-11-30 20:26:47 -0500

    Update hand.py

--- hand.py
+++ hand.py
@@ -1,12 +1,9 @@
 import random
-import deck
-class Hand
+from deuces import deck
+class Hand:
 
     def __init__(self):
-        self.numofcards = 5
         self.hand = []
 
     def deal(self):
-        for i in range(numofcards):
-            newcard = random.choice(Deck)
-            hand += newcard
+        hand.append(deck.draw(5))
