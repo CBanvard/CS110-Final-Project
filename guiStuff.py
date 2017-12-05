e431733d68598ec1cb06f0808125ca1e8e78542a
hurhurhurt <33258519+hurhurhurt@users.noreply.github.com>
2017-12-05 17:27:46 -0600

    Update guiStuff.py

--- guiStuff.py
+++ guiStuff.py
@@ -1,27 +1,32 @@
 import sys, pygame
+from PIL import Image
 
-class MainScreen:
-    def __init__(self):
-        #loads all the reference variables
+class Screen:
+    def __init__(self, location):
+        # these are the rgb values
+        black = (0, 0, 0)
+        white = (255, 255, 255)
+
+        clock = pygame.time.Clock()
+
+        #i reference these multiple times throughout for buttons/images/other things
+        display_width = 1200
+        display_height = 700
+
+        #initialize screen
         pygame.init()
-        self.width = 1200
-        self.height = 700
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.screen = pygame.display.set_mode((display_width, display_height))
         pygame.display.set_caption('Israeli Poker!')
 
-        self.background = pygame.image.load('pokerbackground.png')
-        self.screen.blit(self.background, (0, 0))
-        self.image3 = pygame.image.load('image3.png')
-        self.screen.blit(self.image3, (625, 150))
+        #initialize background
+        self.image = pygame.image.load('Assets/pokerBackground.png')
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+        self.screen.blit(self.image, (0, 0))
+
+        # Displays the welcome text
         self.font = pygame.font.SysFont("Arial", 50)
-        self.font.setbold(True)
-
-    def start_init(self):
-        white = (255, 255, 255)
-        #printsimages
-        self.screen.blit(self.background, (0, 0))
-        self.screen.blit(self.image3, (625, 150))
-
+        self.font.set_bold(True)
         self.text = self.font.render("Welcome to Israeli Poker!", 1, white)
         self.textpos = self.text.get_rect()
         self.textpos.centerx = self.screen.get_rect().centerx
@@ -29,57 +34,36 @@ class MainScreen:
 
         self.TextRect = self.font.render("Play Game!", True, white)
         self.TextRect.get_rect()
-        self.screen.blit(self.TextRect, (200, 200))
+        self.screen.blit(self.TextRect, (200,200))
 
         self.TextRect = self.font.render("Instructions", True, white)
         self.TextRect.get_rect()
-        self.screen.blit(self.TextRect, (200, 300))
+        self.screen.blit(self.TextRect, (200,300))
 
         self.TextRect = self.font.render("Quit Game", True, white)
         self.TextRect.get_rect()
-        self.screen.blit(self.TextRect, (200, 400))
+        self.screen.blit(self.TextRect, (200,400))
 
-    def state_loop(self):
-        self.state = 0
-        while True:
-            if self.state == 0:
-                self.gameloop()
-            elif self.state == 1:
-                self.instructions()
-            elif self.state == 2:
-                sys.exit()
-
-    def main_loop(self):
-        
-
-    def instructions(self):
-        self.screen.blit(self.background, (0, 0))
-        self.image2 = pygame.image.load('instructions.png')
-        self.rect2 = self.image2.get_rect()
-        self.rect2.left, self.rect2.top = location
-        self.screen.blit(self.image2, (150, 75))
-
-        self.TextRect = self.font.render("Return to Menu!", True, white)
-        self.TextRect.get_rect()
-        self.screen.blit(self.TextRect, (400, 200))
-
-'''
-        if 450 > mouse[0] > 200 and 240 > mouse[1] > 200:
-            if rect.collidepoint(200, 200):
-                mainloop()
-
-        if 450 > mouse[0] > 200 and 340 > mouse[1] > 300:
-            if rect.collidepoint(200, 300):
-                instructions()
-
-        if 450 > mouse[0] > 200 and 440 > mouse[1] > 400:
-            if rect.collidepoint(200, 400):
-                sys.exit()
-
+        # Blit everything to the screen
         pygame.display.flip()
 
-'''
+        for event in pygame.event.get():
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
 
-def main():
-        c = MainScreen(300, 300)
-main()
+            #exits the game if user presses button to close window
+            if event.type == pygame.QUIT: sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if 450 > mouse[0] > 200 and 240 > mouse[1] > 200:
+                    print("Play Game! -", mouse)
+                    my_table = table.Table()
+                    my_table.gameFlow()
+                if 450 > mouse[0] > 200 and 340 > mouse[1] > 300:
+                    print("Instructions -", mouse)
+                    img = Image.open('Assets/instructions.png')
+                    img.show()
+                if 450 > mouse[0] > 200 and 440 > mouse[1] > 400:
+                    print("Quit Game -", mouse)
+                    sys.exit()
+
