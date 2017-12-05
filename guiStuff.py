1664b4c8fcf7fce4d058cc5fadea68f90f2c6c2d
hurhurhurt <33258519+hurhurhurt@users.noreply.github.com>
2017-12-05 09:37:42 -0600

    Update guiStuff.py

--- guiStuff.py
+++ guiStuff.py
@@ -3,10 +3,7 @@ import sys, pygame
 class Screen:
     def __init__(self, location):
         # these are the rgb values
-        black = (0, 0, 0)
         white = (255, 255, 255)
-
-        clock = pygame.time.Clock()
 
         #i reference these multiple times throughout for buttons/images/other things
         display_width = 1200
@@ -22,6 +19,13 @@ class Screen:
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
         self.screen.blit(self.image, (0, 0))
+
+        self.image4 = pygame.image.load('cardBack.png')
+
+        self.image3 = pygame.image.load('image3.png')
+        self.rect3 = self.image3.get_rect()
+        self.rect3.left, self.rect3.top = location
+        self.screen.blit(self.image3, (625, 150))
 
         # Displays the welcome text
         self.font = pygame.font.SysFont("Arial", 50)
@@ -43,8 +47,42 @@ class Screen:
         self.TextRect.get_rect()
         self.screen.blit(self.TextRect, (200,400))
 
+        #adds sound
+        sound = pygame.mixer.music.load('Elevator-music.mp3')
+        pygame.mixer.music.play(-1)
+
         # Blit everything to the screen
         pygame.display.flip()
+
+'''
+    def scoreboard(self):
+
+
+
+
+    def play_game(self):
+        self.screen.blit(self.image, (0, 0))
+
+
+
+
+
+
+
+
+
+
+
+    def instructions(self):
+        self.screen.blit(self.image, (0, 0))
+        self.image2 = pygame.image.load('instructions.png')
+        self.rect2 = self.image2.get_rect()
+        self.rect2.left, self.rect2.top = location
+        self.screen.blit(self.image2, (150, 75))
+
+    def results(self):
+'''
+
 
 def main():
     while 1:
@@ -55,12 +93,11 @@ def main():
 
             #exits the game if user presses button to close window
             if event.type == pygame.QUIT: sys.exit()
-
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if 450 > mouse[0] > 200 and 240 > mouse[1] > 200:
                     print("Play Game! -", mouse)
                 if 450 > mouse[0] > 200 and 340 > mouse[1] > 300:
                     print("Instructions -", mouse)
                 if 450 > mouse[0] > 200 and 440 > mouse[1] > 400:
-                    print("Quit Game -", mouse)
+                    sys.exit()
 main()
