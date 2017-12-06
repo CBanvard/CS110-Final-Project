4fb0b1ee3ff732a4c9d74b77fa79417d93b1c1c5
hurhurhurt <33258519+hurhurhurt@users.noreply.github.com>
2017-12-06 14:23:49 -0600

    Update guiStuff.py

--- guiStuff.py
+++ guiStuff.py
@@ -1,5 +1,7 @@
 import sys, pygame
 import tkinter as tk
+import deuces
+import random
 
 class Controller:
     def __init__(self, location):
@@ -47,16 +49,9 @@ class Controller:
 
             #exits the game if user presses button to close window
             if event.type == pygame.QUIT: sys.exit()
-
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if 450 > mouse[0] > 200 and 240 > mouse[1] > 200:
                     print("Play Game! -", mouse)
-                    '''root = tk.Tk()
-                    photo = tk.PhotoImage(file='gameBackground.png')
-                    root.title('Israeli Poker')
-                    label = tk.Label(root, image=photo)
-                    label.pack()
-                    root.mainloop()'''
                     GameScreen((300,300))
                 if 450 > mouse[0] > 200 and 340 > mouse[1] > 300:
                     print("Instructions -", mouse)
@@ -72,27 +67,56 @@ class Controller:
 
 class GameScreen:
     def __init__(self, location):
-        white = (255, 255, 255)
-        pygame.init()
-        display_width = 1200
-        display_height = 700
-        self.screen = pygame.display.set_mode((display_width, display_height))
-        pygame.display.set_caption('Israeli Poker')
-        self.image = pygame.image.load('gameBackground.png')
-        self.screen.blit(self.image, (0, 0))
-        self.font = pygame.font.SysFont("Arial", 50)
-        self.font.set_bold(True)
-        self.TextRect = self.font.render("New Game", True, white)
-        self.screen.blit(self.TextRect, (50, 50))
-        pygame.display.flip()
+        while True:
+            white = (255, 255, 255)
+            display_width = 1200
+            display_height = 700
+            self.screen = pygame.display.set_mode((display_width, display_height))
+            pygame.display.set_caption('Israeli Poker')
+            self.image = pygame.image.load('gameBackground.png')
+            self.screen.blit(self.image, (0, 0))
+            self.font = pygame.font.SysFont("Arial", 30)
+            self.font.set_bold(True)
+            self.TextRect = self.font.render("Back to Main Screen", True, white)
+            self.screen.blit(self.TextRect, (10, 10))
+            self.TextRect = self.font.render("Your hand", True, white)
+            self.screen.blit(self.TextRect, (515, 525))
+            self.TextRect = self.font.render("Their hand", True, white)
+            self.screen.blit(self.TextRect, (510, 115))
+            #pygame.draw.lines(self.image, white, True, (50,50), 1)
+            pygame.display.flip()
 
-        for event in pygame.event.get():
-            #exits the game if user presses button to close window
-            if event.type == pygame.QUIT: sys.exit()
+            for event in pygame.event.get():
+                #exits the game if user presses button to close window
+                mouse = pygame.mouse.get_pos()
+                if event.type == pygame.QUIT: sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print("Quit Game -", mouse)
+                    if 300 > mouse[0] > 10 and 30 > mouse[1] > 10:
+                        while 1:
+                            Controller((300,300))
+
+            imageList = ['2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s', '10c', '10d', '10h', '10s', 'Jc', 'Jd', 'Jh', 'Js', 'Qc', 'Qd', 'Qh', 'Qs', 'Kc', 'Kd', 'Kh', 'Ks', 'Ac', 'Ad', 'Ah', 'As']
+            newList = [c + '.png' for c in imageList]
+            self.image = pygame.image.load(random.choice(newList))
+            self.screen.blit(self.image, (350, 25))
+            self.screen.blit(self.image, (450, 25))
+            self.screen.blit(self.image, (550, 25))
+            self.screen.blit(self.image, (650, 25))
+            self.screen.blit(self.image, (750, 25))
+
+            self.screen.blit(self.image, (350, 600))
+            self.screen.blit(self.image, (450, 600))
+            self.screen.blit(self.image, (550, 600))
+            self.screen.blit(self.image, (650, 600))
+            self.screen.blit(self.image, (750, 600))
+
+            pygame.display.flip()
+
 
 
 def main():
     while 1:
-        c = Controller((300,300))
+        Controller((300,300))
 main()
 
