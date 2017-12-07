23d77708ef1bd389551f5c26c476a10d7965261b
CBanvard <33142678+CBanvard@users.noreply.github.com>
2017-12-06 21:10:49 -0500

    Update table.py

--- table.py
+++ table.py
@@ -7,11 +7,21 @@ import player
 class Table:
     
     def __init__(self):
+        '''
+        Intizilizes the Player class and creates both players
+        param: Self
+        returns: None
+        '''
         self.P1 = player.Player()
         self.P2 = player.Player()
         self.round_num = 0
     
     def gameFlow(self):
+        '''
+        Starts the game, and begins to call all the other classes
+        param: Self
+        returns: None
+        '''
         for i in range(5):
             self.round_num += 1
             self.P1.hand.deal()
@@ -21,6 +31,11 @@ class Table:
                 self.P2.selectCard()
     
     def whoWins(self):
+        '''
+        After the game is finished this is called and it evaulates each collection against the other
+        param: Self, which is each player
+        returns: None
+        '''
         evaluator = Evaluator()
         
         P1_hands_won = 0
