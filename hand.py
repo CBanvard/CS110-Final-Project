714642a4c2b890ec71067323cd2bd55c44de60d8
CBanvard <33142678+CBanvard@users.noreply.github.com>
2017-12-06 20:57:13 -0500

    Update hand.py

--- hand.py
+++ hand.py
@@ -3,7 +3,17 @@ from deuces import deck
 class Hand:
 
     def __init__(self):
+        '''
+        Intizilizes Hand class, which creates the hand for each player
+        param: Self,
+        returns: None
+        '''
         self.hand = []
 
     def deal(self):
+        '''
+        Deals out the cards to each players hand randomly
+        param: Self, and the location of the gui in a tuple
+        returns: None
+        '''
         hand.append(deck.draw(5))
