78512bf7f12283e65fbc836c3d13f747e26e168b
CBanvard <33142678+CBanvard@users.noreply.github.com>
2017-12-06 20:56:14 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -4,10 +4,20 @@ import hand
 class Collection:
 
     def __init__(self):
+        '''
+        Intizilizes the collection class
+        param: Self, and the location of the gui in a tuple
+        returns: None
+        '''
         self.collection = []
         self.already_played = 0
 
     def addCard(self, card):
+        '''
+        Adds cards to the different collections which are columns inside of the game
+        param: Self, and the card that is being played inside of the column
+        returns: None
+        '''
         if table.round_num = already_played:
             self.collection.append(card)
             self.already_played += 1
